75e32c97e2da0d83d1393372cfbcad63fab5fdb9
christinapan98 <christina.pan@wustl.edu>
2025-12-01 22:45:39 -0500

    Add aggregate metrics

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 
 function List({data, nameFilter, dateFilter}: {data: any[], nameFilter: string, dateFilter: number}) {
- 
+  const totals = useMemo(() => {
+    return data.reduce(
+      (acc, curr) => {
+        acc.totalWaterUsage += curr.metrics.Water;
+        acc.totalWaterSavings += curr.savings.Water;
+        acc.totalTimeUsage += curr.metrics.time;
+        acc.totalTimeSavings += curr.savings.time;
+        return acc;
+      },
+      {
+        totalWaterUsage: 0,
+        totalWaterSavings: 0,
+        totalTimeUsage: 0,
+        totalTimeSavings: 0,
+      }
+    );
+  }, [data]);
+
+  const {
+    totalWaterUsage,
+    totalWaterSavings,
+    totalTimeUsage,
+    totalTimeSavings,
+  } = totals;
+
   return (
     <div>
+      <div>
+        <div>Aggregate Metrics for {nameFilter === "" ? "All Tanks " : nameFilter} in the last {dateFilter} days</div>
+        <span>Total Water Used: {totalWaterUsage}</span>
+        <span>Total Water Saved: {totalWaterSavings}</span>
+        <span>Total Time Taken: {totalTimeUsage}</span>
+        <span>Total Time Saved: {totalTimeSavings}</span>
+      </div>
+
       {
         data.map((item) => (
           <div key={item.id} className="data-item">
